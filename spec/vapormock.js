8baadbc62b049e0bf01d66c864058827360d7a7c
Michael Scholtz <michael.scholtz@outlook.com>
2015-09-20 10:27:18 +0200

    fix vapor mock

--- spec/vapormock.js
+++ spec/vapormock.js
@@ -2,13 +2,13 @@
  * Vapor mock object used for testing
  */
 module.exports = {
-    config: {
+    _config: {
         admins: ['7656'],
         logs: {
             dateFormat: 'YYYY-MM-DD HH:mm:ss'
         }
     },
-    steamFriends: {
+    _steamFriends: {
         personaStates: {
             '7656': {
                 player_name: 'vapor'
